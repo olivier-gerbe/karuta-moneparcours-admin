1deb6186108a478e3978dc8c9a45c1a8e6a4a355
olivier.gerbe <olivier.gerbe@hec.ca>
2019-10-17 16:23:00 -0400

    update

---
+++ WebContent/application/js/specific.js
@@ -0,0 +1,20 @@
+
+//------ EXEC BATCH AT USER CREATION ------------------
+var g_execbatch = true;
+var g_execbatchbuttonlabel1 = [];
+	g_execbatchbuttonlabel1['fr'] = "Patience! Création de votre portfolio ...";
+var g_json = {};
+
+//=======================
+function prepareBatch()
+//=======================
+{
+	g_json['model_code'] = "modeles-cnam.batch-creation";
+	g_json['lines'] = [];
+	g_json.lines[0] =
+	{
+		"auditeurCourriel" : USER.email,
+		"auditeurNomFamille" : USER.lastname,
+		"auditeurPrenom" : USER.firstname,
+	};
+}//----------------------------------------------------
